7e33c6d6079e8cce977ee1927c378cc2cf99e013
veritas0806 <veritas0806@gmail.com>
2020-11-04 18:00:11 +0900

    Init!

---
+++ src/test/java/com/bundesreport/BundesReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bundesreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BundesReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
