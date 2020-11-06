e1923534b70eb31006a4da26711639166494c5a1
veritas0806 <veritas0806@gmail.com>
2020-11-06 09:23:55 +0900

    #0011: Multi Language

--- src/main/java/com/bundesreport/controller/PageController.java
+++ src/main/java/com/bundesreport/controller/PageController.java
@@ -1,13 +1,22 @@
 package com.bundesreport.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
+	@Autowired
+	MessageSource messageSource;
+
 	@RequestMapping(value = "/")
 	public String home() {
+		Locale.setDefault(Locale.ROOT);
+		System.out.println(messageSource.getMessage("title", new String[] { "title" }, Locale.getDefault()));
 		return "board.html";
 	}
 }
