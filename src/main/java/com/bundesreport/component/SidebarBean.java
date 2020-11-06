3c4f5318ea140408868f71ba3ebab67cfd787f04
veritas0806 <veritas0806@gmail.com>
2020-11-06 10:45:27 +0900

    #0012: SidebarBean: Add SidebarBean

---
+++ src/main/java/com/bundesreport/component/SidebarBean.java
@@ -0,0 +1,61 @@
+package com.bundesreport.component;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+
+import com.bundesreport.util.MessageUtil;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SidebarBean {
+
+	public SidebarBean(MessageSource messageSource, Locale locale) {
+		if (locale.equals(null)) {
+			locale = Locale.ROOT;
+		}
+		MessageUtil msgUtil = new MessageUtil();
+		title = msgUtil.getMessage(messageSource, "title", locale);
+		home = msgUtil.getMessage(messageSource, "menu.home", locale);
+		menu = msgUtil.getMessage(messageSource, "menu.menu", locale);
+		board = msgUtil.getMessage(messageSource, "menu.board", locale);
+		freeBoard = msgUtil.getMessage(messageSource, "menu.freeBoard", locale);
+		abroadBoard = msgUtil.getMessage(messageSource, "menu.abroadBoard", locale);
+		livingQA = msgUtil.getMessage(messageSource, "menu.livingQA", locale);
+		fleaMarket = msgUtil.getMessage(messageSource, "menu.fleaMarket", locale);
+		jobSearch = msgUtil.getMessage(messageSource, "menu.jobSearch", locale);
+		club = msgUtil.getMessage(messageSource, "menu.club", locale);
+		eventNotification = msgUtil.getMessage(messageSource, "menu.eventNotification", locale);
+		recipe = msgUtil.getMessage(messageSource, "menu.recipe", locale);
+		gallery = msgUtil.getMessage(messageSource, "menu.gallery", locale);
+	}
+
+	private String title;
+
+	private String home;
+
+	private String menu;
+
+	private String board;
+
+	private String freeBoard;
+
+	private String abroadBoard;
+
+	private String livingQA;
+
+	private String fleaMarket;
+
+	private String jobSearch;
+
+	private String club;
+
+	private String eventNotification;
+
+	private String recipe;
+
+	private String gallery;
+}
