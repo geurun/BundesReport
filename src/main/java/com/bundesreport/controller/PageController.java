f6d41097d452a9226db760199521d9af406f8e5a
veritas0806 <veritas0806@gmail.com>
2020-11-06 10:53:39 +0900

    #0012: PageController: Add Sidebar to Model

--- src/main/java/com/bundesreport/controller/PageController.java
+++ src/main/java/com/bundesreport/controller/PageController.java
@@ -5,7 +5,10 @@ import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bundesreport.component.SidebarBean;
 
 @Controller
 public class PageController {
@@ -14,9 +17,15 @@ public class PageController {
 	MessageSource messageSource;
 
 	@RequestMapping(value = "/")
-	public String home() {
-		Locale.setDefault(Locale.ROOT);
-		System.out.println(messageSource.getMessage("title", new String[] { "title" }, Locale.getDefault()));
+	public String home(Model model) {
+		model = createSidebar(model);
 		return "home.html";
+	}
+
+	private Model createSidebar(Model model) {
+		// ToDo: Use User's initial Language or Selected Language
+		SidebarBean sidebar = new SidebarBean(messageSource, Locale.ROOT);
+		model.addAttribute("sidebar", sidebar);
+		return model;
 	}
 }
