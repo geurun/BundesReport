13ade4bb7d857ce61d7f917e56958f5a5f4b4a47
YouJin Lee <dev-ujin518@gmail.com>
2020-11-10 16:55:39 +0900

    #0022: PostServiceTest: Find all posts

--- src/test/java/com/bundesreport/PostServiceTest.java
+++ src/test/java/com/bundesreport/PostServiceTest.java
@@ -2,6 +2,8 @@ package com.bundesreport;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.junit.Test;
@@ -35,5 +37,31 @@ public class PostServiceTest {
 		//then
 		assertEquals(post, postRepository.findOne(postId));
 		assertEquals("제목1", postRepository.findOne(postId).getTitle());
+		
+	}
+	
+	@Test
+	public void 포스트_모두_조회() throws Exception {
+		//given
+		Post post2 = new Post();
+		Post post3 = new Post();
+		post2.setTitle("제목2");
+		post2.setContent("내용2입니다내용2입니다내용2입니다내용2입니다내용2입니다내용2입니다내용2입니다내용2입니다내용2입니다");
+		post3.setTitle("제목3");
+		post3.setContent("내용3입니다내용3입니다내용3입니다내용3입니다내용3입니다내용3입니다");
+		postRepository.save(post2);
+		postRepository.save(post3);
+
+		
+		//when
+		List<Post> postsAll = postService.findPosts();
+		
+		//then
+		int i=0;
+		for (Post post : postsAll) {
+			i++;
+			System.out.println("" + post.getId() + post.getTitle());
+		}
+		assertEquals(2, i);
 	}
 }
