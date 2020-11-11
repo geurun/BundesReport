055b2c0a5341f2343f39dfbe5babb722b2addffa
veritas0806 <veritas0806@gmail.com>
2020-11-11 09:18:09 +0900

    #0027: LanguageStatus: Add method: getLocale

--- src/main/java/com/bundesreport/type/LanguageStatus.java
+++ src/main/java/com/bundesreport/type/LanguageStatus.java
@@ -1,5 +1,20 @@
 package com.bundesreport.type;
 
+import java.util.Locale;
+
 public enum LanguageStatus {
-	KO, DE
+	KO("KOREAN"), DE("DEUTSCH");
+
+	private String value;
+
+	LanguageStatus(String value) {
+		this.value = value;
+	}
+
+	public Locale getLocale() {
+		if (this.value == "KOREAN") {
+			return Locale.KOREAN;
+		}
+		return Locale.GERMAN;
+	}
 }
