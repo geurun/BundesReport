bd34e39c4010f39958cac48e1966518fa35e81d4
veritas0806 <veritas0806@gmail.com>
2020-11-19 17:31:34 +0900

    Ready for release

--- src/main/java/com/bundesreport/BundesReportApplication.java
+++ src/main/java/com/bundesreport/BundesReportApplication.java
@@ -2,10 +2,17 @@ package com.bundesreport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BundesReportApplication {
+public class BundesReportApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(BundesReportApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BundesReportApplication.class);
 	}
 }
