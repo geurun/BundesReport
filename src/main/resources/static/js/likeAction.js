7c88ec09508cb2e4dca24f16883ed0df33ab0c80
veritas0806 <veritas0806@gmail.com>
2020-11-23 12:54:20 +0900

    #0061: Add Comment Modify

--- src/main/resources/static/js/likeAction.js
+++ src/main/resources/static/js/likeAction.js
@@ -28,3 +28,44 @@ $("#btnPostLike").on("click", function() {
 		}
 	});
 });
+
+$(".btn").click(function() {
+	var btnId = $(this).attr("id");
+	if (btnId.indexOf("btnModify") != -1) {
+		var num = btnId.substring(9);
+		var txtComment = $("#txtComment" + num);
+		if (txtComment.attr("readonly")) {
+			txtComment.attr("readonly", false);
+		} else {
+			commentModifyAction($(btnId), txtComment, num);
+		}
+	}
+});
+
+function commentModifyAction(btnModify, txtComment, num) {
+	event.preventDefault();
+	btnModify.prop("disabled", true);
+
+	var commentForm = {};
+	commentForm["id"] = num;
+	commentForm["content"] = txtComment.val();
+
+	$.ajax({
+		type: "POST",
+		contentType: "application/json",
+		url: "/comment/update",
+		data: JSON.stringify(commentForm),
+		dataType: 'json',
+		cache: false,
+		timeout: 600000,
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader($("meta[name='_csrf_header']").attr("content"), $("meta[name='_csrf']").attr("content"));
+		}, success: function(data) {
+			txtComment.attr("readonly", true);
+			btnModify.prop("disabled", false);
+		}, error: function(e) {
+			txtComment.attr("readonly", true);
+			btnModify.prop("disabled", false);
+		}
+	});
+};
