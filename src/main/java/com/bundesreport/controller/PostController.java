9ad503724fa04815487cc02ca0db7be68ee211d3
YouJin Lee <dev.ujin518@gmail.com>
2020-11-23 17:10:06 +0900

    #0062: Make getFirstImage in Post domain

--- src/main/java/com/bundesreport/controller/PostController.java
+++ src/main/java/com/bundesreport/controller/PostController.java
@@ -123,4 +123,17 @@ public class PostController extends PageController {
 	public String delete(@PathVariable("postId") Long postId) {
 		return "redirect:/post/list/" + postService.delete(postId);
 	}
+	
+	@GetMapping(value = "/gallery/list")
+	public String gallery(Model model, Authentication auth) {
+		try {
+			model = createLayout(model, auth);
+			String category = "gallery";
+			model.addAttribute("bean",
+					new PostListBean(msgSrc, auth, postService.findByCategory(category), category, true));
+			return "post/gallery";
+		} catch (Exception e) {
+			return "redirect:/404";
+		}
+	}
 }
