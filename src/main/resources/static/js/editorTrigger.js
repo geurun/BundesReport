c454cabdc6ed47ab59d7d7286240ae66c65fb4f6
veritas0806 <veritas0806@gmail.com>
2020-11-25 13:54:27 +0900

    [bugfix] set image max-size

--- src/main/resources/static/js/editorTrigger.js
+++ src/main/resources/static/js/editorTrigger.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-  $('#editor').summernote({
-    toolbar: [
-      ['fontname', ['fontname']],
-      ['fontsize', ['fontsize']],
-      ['font', ['bold', 'underline', 'clear']],
-      ['color', ['color']],
-      ['para', ['ul', 'ol', 'paragraph']],
-      ['table', ['table']],
-      ['insert', ['picture']],
-	],
-	height: 400,
-  });
+	$('#editor').summernote({
+		toolbar: [
+			['fontname', ['fontname']],
+			['fontsize', ['fontsize']],
+			['font', ['bold', 'underline', 'clear']],
+			['color', ['color']],
+			['para', ['ul', 'ol', 'paragraph']],
+			['table', ['table']],
+			['insert', ['picture']],
+		],
+		height: 400,
+		maximumImageFileSize: 2097152,
+		callbacks: {
+			onImageUploadError: function(msg) {
+				alert('Change image! (Max: 2MB)');
+			}
+		}
+	});
 });
